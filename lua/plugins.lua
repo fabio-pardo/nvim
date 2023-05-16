e92bb29ace5918f130f0974dcf2faabbf1297c42
Fabio Pardo <fpardo1023@gmail.com>
2023-05-16 14:58:02 -0400

    add treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,18 +2,18 @@ return {
 
   { "lewis6991/impatient.nvim" },
   { "ellisonleao/gruvbox.nvim", priority = 1000 },
-  { "christoomey/vim-tmux-navigator"},
+  { "christoomey/vim-tmux-navigator", lazy = false},
   {
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
     dependencies = {
-      'nvim-treesitter/nvim-treesitter-refactor',
-      'RRethy/nvim-treesitter-textsubjects',
-      'RRethy/nvim-treesitter-endwise',
+      "nvim-treesitter/nvim-treesitter-refactor",
+      "RRethy/nvim-treesitter-textsubjects",
+      "RRethy/nvim-treesitter-endwise",
     },
-    build = ':TSUpdate',
-    event = 'VeryLazy',
-    --config = function()
-    --  require 'config.treesitter'
-    --end,
+    build = ":TSUpdate",
+    event = "VeryLazy",
+    config = function()
+      require "config.treesitter"
+    end,
   },
 }
